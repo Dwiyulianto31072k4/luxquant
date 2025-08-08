745053e0dc53be5f7792d71ffa0b151503a3b3ba
Dwi Yulianto <110501342+Dwiyulianto31072k4@users.noreply.github.com>
2025-08-08 16:37:10 +0700

    Update referral.js

--- referral.js
+++ referral.js
@@ -7,7 +7,7 @@ class ReferralSystem {
     this.validKolCodes = [
       '03451Lux', '02847Lux', '05632Lux', 
       '01258Lux', '09874Lux', '04569Lux',
-      'KOL001Lux', 'KOL002Lux', 'BHYUKNGLO'
+      'KOL001Lux', 'MRCOWHALE', 'BHYUKNGLO'
     ];
     this.originalPrice = 79.99;
     this.discountPercent = 12.5;
