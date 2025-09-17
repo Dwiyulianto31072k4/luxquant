37dd98fb8ad328dc7c04cfd1610d5066fd78e267
Dwi Yulianto <110501342+Dwiyulianto31072k4@users.noreply.github.com>
2025-09-17 07:33:54 +0700

    Update referral.js

--- referral.js
+++ referral.js
@@ -9,7 +9,7 @@ class ReferralSystem {
       '01258Lux', '09874Lux', '04569Lux',
       'SM138', 'MRCOWHALE', 'BHYUKNGLO', 'FARIDWHALEBTC', 'ERIKDI', 'thomatomb', 'tss07', 'brndn', 'febrian'
     ];
-    this.originalPrice = 200;
+    this.originalPrice = 300;
     this.discountPercent = 12.5;
     this.discountPrice = this.originalPrice * (1 - this.discountPercent / 100);
     this.init();
