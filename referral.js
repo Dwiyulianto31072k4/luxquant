7ae10e0c7e85ff0ee8a0b1a9a6c5597d46d2293e
Dwi Yulianto <110501342+Dwiyulianto31072k4@users.noreply.github.com>
2025-10-24 15:33:13 +0700

    Update referral.js

--- referral.js
+++ referral.js
@@ -7,7 +7,7 @@ class ReferralSystem {
     this.validKolCodes = [
       '03451Lux', '02847Lux', '05632Lux', 
       '01258Lux', '09874Lux', '04569Lux',
-      'SM138', 'MRCOWHALE', 'BHYUKNGLO', 'FARIDWHALEBTC', 'ERIKDI', 'thomatomb', 'tss07', 'brndn', 'febrian', 'crossteam', 'Neilsen', 'CYBRFI', 'ejeha', 'ndratraderalgo'
+      'SM138', 'MRCOWHALE', 'BHYUKNGLO', 'FARIDWHALEBTC', 'ERIKDI', 'thomatomb', 'tss07', 'brndn', 'febrian', 'crossteam', 'Neilsen', 'CYBRFI', 'ejeha', 'ndratraderalgo', 'DODO'
     ];
     this.originalPrice = 350;
     this.discountPercent = 5;
